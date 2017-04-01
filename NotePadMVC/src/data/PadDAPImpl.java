a7b015bb2e6d9db9ce933aa965d6df0068795b92
DanRezo <DrEzO@DaNReZO.local>
2017-04-01 12:49:08 -0600

    Added skeleton layout of Pad Controllers Impls Dao

--- NotePadMVC/src/data/PadDAPImpl.java
+++ NotePadMVC/src/data/PadDAPImpl.java
@@ -1,5 +1,65 @@
 package data;
 
+import java.util.List;
+
+import entities.Playlist;
+import entities.Song;
+import entities.User;
+
 public class PadDAPImpl  implements PadDAO{
+
+	@Override
+	public User editPadName(User alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User addNote(User newNote) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getSongByArtist(Song artist) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getSongByTitle(Song title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getSongsByGenre(Song genere) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User delteSongfromPad(Song pad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User deleteSongFromPlaylist(Song plaulist) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Playlist> playlist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Song> listsongs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
