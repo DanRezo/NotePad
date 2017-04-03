87a8993075be0e76530c00a17d16f9e69b2f2325
Matt Gmur <mattgmur@SDMacBookPro2.local>
2017-04-03 15:31:14 -0600

    merging file

--- NotePadMVC/src/controllers/PadController.java
+++ NotePadMVC/src/controllers/PadController.java
@@ -3,15 +3,21 @@ package controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import entities.User;
 
 @Controller
+@SessionAttributes({"user"})
 public class PadController{
 	
 
 	@RequestMapping(value="test.do")
-	public void test(@ModelAttribute("user") User user){
+	public ModelAndView test(@ModelAttribute("user") User user){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("note");
 		System.out.println(user);
+		return mv;
 	}
 }
